6792544a70338579aa7e782aaad27edaa584bf69
WARATEP TUBBOON <waratep_tubboon@icloud.com>
2019-08-11 03:38:17 +0700

    Add files via upload

--- mototcontrol.py
+++ mototcontrol.py
@@ -16,37 +16,75 @@ p2 = IO.PWM(13,100)
 p.start(0)                              
 p2.start(0)         
 
+avg_motor1 = []
+avg_motor2 = []
+
 
 def map( x,  in_min,  in_max,  out_min,  out_max):
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
+def initQueue():
+    for i in range(10):
+        avg_motor1.append(0)
+        avg_motor2.append(0)
+
+def calavg(data):
+    x = 0
+    for i in range(10):
+        x += data[i]
+    x = x / 10
+    return x
+
+def motor1(x,y):
+    avg_motor1.pop(0)
+    avg_motor1.append(x)
+    if y < 0:
+        x = int(map(calavg(avg_motor1),0,255,0,100) - abs(y))
+    else:
+        x = int(map(calavg(avg_motor1),0,255,0,100))
+    if x > 100: 
+        x = 100
+    if x < 0:
+        x = 0
+
+    p.ChangeDutyCycle(x)
+
+def motor2(x,y):
+    avg_motor2.pop(0)
+    avg_motor2.append(x)
+
+    if y > 0:
+        x = int(map(calavg(avg_motor2),0,255,0,100) - abs(y))
+    else:
+        x = int(map(calavg(avg_motor1),0,255,0,100))
+
+    if x > 100: 
+        x = 100
+    if x < 0:
+        x = 0
+    p2.ChangeDutyCycle(x)
+    
 
 if __name__ == "__main__":
-
+    
+    initQueue()
+    m1 = m2 = m3 = 0
+    
     try:
         while 1:
             for event in dev.read_loop():
+                motor1(m2,m3)
+                motor2(m2,m3)
                 if (event.type == ecodes.EV_ABS):
-                    if event.code == 3: #break_left_motor
-                        print(event.value)
-                        p.ChangeDutyCycle(int(map(event.value,0,255,0,100)))
+                    if event.code == 3: 
+                        m1 = event.value
         
-                    if event.code == 4: #break_right_motor
-                        print(event.value)
-                        p2.ChangeDutyCycle(int(map(event.value,0,255,0,100)))
+                    if event.code == 4: 
+                        m2 = event.value
 
-                    # if event.code == 1:  #forward-left_motor
-                    #     if event.value < 120:
-                    #         print(event.value)
-                    #     if event.value > 140:
-                    #         print(event.value)
-        
-                    # if event.code == 5:  #forward-right_motor
-                    #     if event.value < 120:
-                    #         print(event.value)
-                        
-                    #     if event.value > 140:
-                    #         print(event.value)
+                    if event.code == 0: 
+                        m3 = map(event.value,0,255,-100,100)
+
 
 
     except KeyboardInterrupt:
